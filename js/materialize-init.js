52c2f1fbf2ad7a4cba54edd9b94963d819518e0e
Ryan O'Connor <rocsteady888@gmail.com>
2018-11-10 10:40:41 -0500

    no longer using collapsible buttons

--- js/materialize-init.js
+++ js/materialize-init.js
@@ -14,8 +14,3 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems);
-});
